445208add0a3116e91f57f298d77085ef927a7ce
Rodrigo Ramos <rodrigo.ramos.santos@gmail.com>
2013-11-18 16:41:10 -0200

    Vim version changed

--- posh-vim.ps1
+++ posh-vim.ps1
@@ -4,7 +4,7 @@ function edit ()
 {
   $message = "$nl Vim is already running. $nl Opening with '--remote switch' $nl"; 
   $param = $args;
-  $exe = "C:\Program Files (x86)\Vim\vim73\gvim.exe";
+  $exe = "C:\Program Files (x86)\Vim\vim74\gvim.exe";
   if (get-process | ?{$_.path -eq $exe})
   {
 	Write-host $message -foregroundcolor $warnColor;
